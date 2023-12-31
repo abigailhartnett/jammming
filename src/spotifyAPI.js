a9d6bc3c7a2371e0a04e7e9eef057f7886e4c61a
Abigail Hartnett <abigailhhartnett@gmail.com>
2023-12-31 08:36:25 -0700

    Add checks to API, temporarily block linting error

--- src/spotifyAPI.js
+++ src/spotifyAPI.js
@@ -1,40 +1,57 @@
 async function SpotifyAPI(searchQuery, setAlbums, albums) {
   // Future note: Handle cases where "access_token" is undefined or falsy
-  const accessToken = localStorage.getItem("accessToken");
-  const CLIENT_ID = "efabbdc785874423992878f942b0916d";
-  const CLIENT_SECRET = "2cef9e0db446469b8bb61e9a2cc62b55";
+  try {
+    const accessToken = localStorage.getItem("accessToken");
 
-  // Get request using search to get the Aritist ID
-  const searchParameters = {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + accessToken,
-    },
-  };
-  const artistID = await fetch(
-    "https://api.spotify.com/v1/search?q=" + searchQuery + "&type=artist",
-    searchParameters
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      return data.artists.items[0].id;
-    });
+    // Get request using search to get the Aritist ID
+    const searchParameters = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + accessToken,
+      },
+    };
+    const artistID = await fetch(
+      "https://api.spotify.com/v1/search?q=" + searchQuery + "&type=artist",
+      searchParameters
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.artists) {
+          if (data.artists.items) {
+            // console.log(data.artists.items[0]);
+            return data.artists.items[0].id;
+          }
+        }
+      })
+      .catch((error) => {
+        console.error("Call 1 - Error fetching data from Spotify:".error);
+      });
 
-  // Get request with Artist ID, grab all albums from artist
-  const returnedAlbums = await fetch(
-    "https://api.spotify.com/v1/artists/" +
-      artistID +
-      "/albums" +
-      "?include_groups=album&market=US&limit=50",
-    searchParameters
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      setAlbums(data.items);
-    });
+    // Get request with Artist ID, grab all albums from artist
 
-  // Display those albums to the user
+    //eslint-disable-next-line no-unused-vars
+    const _returnAlbums = await fetch(
+      "https://api.spotify.com/v1/artists/" +
+        artistID +
+        "/albums" +
+        "?include_groups=album&market=US&limit=50",
+      searchParameters
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.items) {
+          setAlbums(data.items);
+        }
+      })
+      .catch((error) => {
+        console.error("Call 2 - Error fetching data from Spotify:", error);
+      });
+
+    // Display those albums to the user
+  } catch (error) {
+    console.error("Call 3 - Error fetching data from Spotify:".error);
+  }
 }
 
 export default SpotifyAPI;
