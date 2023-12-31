38ca3e8d5d9590650f1fe4b63ac0dd0390a29ca4
Abigail Hartnett <abigailhhartnett@gmail.com>
2023-12-31 14:30:03 -0700

    Update search bar functionality

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,23 @@
 import SpotifyAPI from "../spotifyAPI";
 
 const SearchBar = ({ setTracks, setSearchQuery, searchQuery }) => {
-  async function handleSearch() {
+  async function handleSearch(e) {
+    e.preventDefault();
     await SpotifyAPI(searchQuery, setTracks);
   }
 
   return (
-    <div className="search-bar">
-      <input
-        name="search"
-        id="search"
-        type="text"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-      />
-      <button onClick={() => handleSearch(searchQuery)}>Search</button>
+    <div>
+      <form className="search-bar" onSubmit={handleSearch}>
+        <input
+          name="search"
+          id="search"
+          type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
     </div>
   );
 };
