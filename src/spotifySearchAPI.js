1d6231445ae8166179a3676bc1e8335d4db39bd5
Abigail Hartnett <92175468+abigailhartnett@users.noreply.github.com>
2024-01-06 14:35:21 -0700

    Reformat spotifySearchAPI

--- src/spotifySearchAPI.js
+++ src/spotifySearchAPI.js
@@ -1,46 +1,37 @@
 async function SpotifySearchAPI(searchQuery, setTracks, tracks) {
-  // Future note: Handle cases where "access_token" is undefined or falsy
+  const accessToken = localStorage.getItem("accessToken");
+  const apiURL = "https://api.spotify.com/v1/search?q="
+
+  const request = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + accessToken,
+    },
+  };
+
   try {
-    const accessToken = localStorage.getItem("accessToken");
-
-    // Get request using search to get the data
-    const searchParameters = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + accessToken,
-      },
-    };
-
-    //eslint-disable-next-line no-unused-vars
-    const _trackList = await fetch(
-      "https://api.spotify.com/v1/search?q=" + searchQuery + "&type=track",
-      searchParameters
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.tracks && data.tracks.items && data.tracks.items.length > 0) {
-          // Use returned data to setTracks to an array of objects containing only the data I want
-          const tracksData = data.tracks.items.map((track) => {
-            // console.log(track.uri); -- delete me
-            return {
-              id: track.id,
-              name: track.name,
-              album: track.album.name,
-              artist: track.artists.map((artist) => artist.name).join(", "),
-              uri: track.uri,
-            };
-          });
-          setTracks(tracksData);
-        } else {
-          console.log("No tracks found");
-        }
-      })
-      .catch((error) => {
-        console.error(
-          "Track data call - Error fetching data from Spotify:".error
-        );
-      });
+    const response = await fetch(apiURL + searchQuery + "&type=track", request)
+      
+    if (response.ok) {
+      const data = await response.json();
+      if (data.tracks && data.tracks.items && data.tracks.items.length > 0) {
+        // Use returned data to setTracks to an array of objects containing only the data I want
+        const tracksData = data.tracks.items.map((track) => {
+          // console.log(track.uri); -- delete me
+          return {
+            id: track.id,
+            name: track.name,
+            album: track.album.name,
+            artist: track.artists.map((artist) => artist.name).join(", "),
+            uri: track.uri,
+          };
+        });
+        setTracks(tracksData);
+      } else {
+        console.log("No tracks found");
+      }
+    }
   } catch (error) {
     console.error("API call - Error fetching data from Spotify:".error);
   }
