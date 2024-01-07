e914e06a2ad6d4d89f4f3ce73311bc55467437ab
Abigail Hartnett <92175468+abigailhartnett@users.noreply.github.com>
2024-01-07 10:12:58 -0700

    Change redirect to live site

--- src/SpotifyAuth.js
+++ src/SpotifyAuth.js
@@ -1,8 +1,8 @@
 import { useEffect } from "react";
 
 const CLIENT_ID = "efabbdc785874423992878f942b0916d";
-const REDIRECT_URI = "http://localhost:3000";
-// const REDIRECT_URI = "https://abigailhartnett-codecademy-jammming.netlify.app";
+// const REDIRECT_URI = "http://localhost:3000";
+const REDIRECT_URI = "https://abigailhartnett-codecademy-jammming.netlify.app";
 const SCOPES = [
   "playlist-modify-private",
   "playlist-modify-public",
